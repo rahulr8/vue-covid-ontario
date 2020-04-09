e3a905a8a6b615807cbc8f4e4761c378272cba5f
Rahul <rahuliodev8@gmail.com>
2020-04-09 18:38:07 -0400

    Add sample data

---
+++ src/constants/sampleData.js
@@ -0,0 +1,18 @@
+export const NUM_RESOLVED = 1000;
+export const NUM_FATALITIES = 1000;
+export const NUM_ACTIVE = 4000;
+
+export const NUM_MALES = 300;
+export const NUM_FEMALES = 2000;
+
+export const NUM_CASES_BELOW_10 = 400;
+export const NUM_CASES_10_TO_20 = 600;
+export const NUM_CASES_20_TO_30 = 300;
+export const NUM_CASES_30_TO_40 = 500;
+export const NUM_CASES_40_TO_50 = 100;
+export const NUM_CASES_50_TO_60 = 500;
+export const NUM_CASES_60_TO_70 = 100;
+export const NUM_CASES_70_TO_80 = 800;
+export const NUM_CASES_80_TO_90 = 400;
+export const NUM_CASES_90_TO_100 = 600;
+export const NUM_CASES_OVER_100 = 40;
